b1b0db9fa759b89920c24f606d03ad40b24bf386
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2020-01-12 15:30:52 +0100

    Better plot
    Add doc
    
    No default lat lon

--- data_loader/data_plot.py
+++ data_loader/data_plot.py
@@ -81,20 +81,44 @@ class DataPlot(DataBase):
         ax.set_xlim(*self.coords[names[0]].get_limits(kw_coords[names[0]]))
         ax.set_ylim(*self.coords[names[1]].get_limits(kw_coords[names[1]]))
 
-    def imshow(self, ax, variable=None, limits=True, kwargs=None, **kw_coords):
-        """Plot a frame on a heatmap."""
+    def guess_map_coords(self, kw_coords):
+        """Find coordinates with keys of size higher than one."""
+        coords = [name for name, key in kw_coords.key()
+                  if self.coords[name][key].size > 1]
+        return coords
+
+    def imshow(self, ax, variable, coords=None, limits=True, kwargs=None, **kw_coords):
+        """Plot a frame on a heatmap.
+
+        Parameters
+        ----------
+        ax: Matplotlib axis
+        variable: str
+        coords: List[str], optional
+            Coordinate to plot along.
+            If None, selected coordinates with size higher
+            than 1 will be used.
+        limits: bool, optional
+            Set axis limits.
+        kwargs: Dict[Any]
+            Passed to imshow.
+        kw_coords: Keys
+            Subset of data to plot.
+
+        Returns
+        -------
+        Matplotlib image.
+        """
         self._check_loaded()
 
-        if variable is None:
-            variable = self.vi.var[0]
-
         kw_coords = self.get_coords_full(**kw_coords)
-        keys_latlon = self.get_none_total(**{k: kw_coords[k]
-                                             for k in ['lat', 'lon']})
-        kw_coords.update(keys_latlon)
-        kw_coords = self._get_none_single(**kw_coords)
-        kw_coords = self.sort_by_coords(kw_coords)
+        if coords is not None:
+            keys_hor = self.get_coords_none_total(**{k: kw_coords[k]
+                                                     for k in coords})
+            kw_coords.update(keys_hor)
+        kw_coords = self._get_coords_none_single(**kw_coords)
 
+        frame = self.select(variable, **kw_coords)
         self.set_plot_keys(variable, **kw_coords)
 
         kwargs_def = {'origin': 'lower'}
@@ -102,15 +126,15 @@ class DataPlot(DataBase):
             kwargs_def.update(kwargs)
         kwargs = kwargs_def
 
-        slice_lat = kw_coords['lat']
-        slice_lon = kw_coords['lon']
-        extent = self.lon.get_extent(slice_lat) + self.lat.get_extent(slice_lon)
-        frame = self[variable][tuple(self.slices_plot.values())]
+        if coords is None:
+            coords = self.guess_map_coords(kw_coords)
+        slices = [kw_coords[name] for name in coords]
+        extent = self.get_extent(dict(zip(coords, slices)))
 
         im = ax.imshow(frame, extent=extent, **kwargs)
 
         if limits:
-            self.set_limits(ax, slice_lon, slice_lat)
+            self.set_limits(ax, slices)
 
         return im
 
@@ -119,66 +143,133 @@ class DataPlot(DataBase):
 
         If a parameter is None, the value used for setting
         the plot is used.
+
+        Parameters
+        ----------
+        im: Matplotlib image
+        variable: str
+            If None, last plotted is used.
+        kw_coords: Keys
+            Subset of data to plot.
+            If missing, last plotted is used.
         """
         if variable is None:
             variable = self.last_plot
         kw_coords = self.get_coords_full(**kw_coords)
-        kw_coords = self.sort_by_coords(kw_coords)
-
         kw_coords = self._get_coords_none_previous(**kw_coords)
         self.set_plot_keys(variable, **kw_coords)
 
-        frame = self[variable][tuple(kw_coords.values())]
+        frame = self.select(variable, **kw_coords)
         im.set_data(frame)
 
-    def contour(self, ax, variable=None, limits=True, kwargs=None, **kw_coords):
-        if variable is None:
-            variable = self.vi.var[0]
+    def contour(self, ax, variable, coords=None, limits=True, kwargs=None, **kw_coords):
+        """Plot contour of a variable.
+
+        Parameters
+        ----------
+        ax: Matplotlib axis
+        variable: str
+        coords: List[str], optional
+            Coordinates to plot along to.
+            If None, selected coordinates with dimension higher
+            than 1 will be used.
+        limits: bool, optional
+            Set axis limits.
+        kwargs: Dict[Any]
+            Passed to contour.
+        kw_coords: Keys
+            Subset of data to plot.
+
+        Returns
+        -------
+        Matplotlib contour
+        """
         kw_coords = self.get_coords_full(**kw_coords)
-        keys_latlon = self.get_none_total(**{k: kw_coords[k]
-                                             for k in ['lat', 'lon']})
-        kw_coords.update(keys_latlon)
-        kw_coords = self._get_none_single(**kw_coords)
-        kw_coords = self.sort_by_coords(kw_coords)
+        if coords is not None:
+            keys_hor = self.get_coords_none_total(**{k: kw_coords[k]
+                                                     for k in coords})
+            kw_coords.update(keys_hor)
+        kw_coords = self._get_coords_none_single(**kw_coords)
 
         self.set_plot_keys(variable, **kw_coords)
+        frame = self.select(variable, **kw_coords)
 
-        slice_lat = kw_coords['lat']
-        slice_lon = kw_coords['lon']
-        frame = self[variable][tuple(kw_coords.values())]
-        c = ax.contour(self.lon[slice_lon], self.lat[slice_lat],
-                       frame, **kwargs)
+        if coords is None:
+            coords = self.guess_map_coordinates(kw_coords)
+        slices = [kw_coords[name] for name in coords]
+        values = [self.coords[name][kw_coords[name]]
+                  for name in coords]
+
+        c = ax.contour(*values, frame, **kwargs)
         if limits:
-            self.set_limits(ax, slice_lon, slice_lat)
+            self.set_limits(ax, *slices)
 
         return c
 
-    def update_contour(self, ax, c, variable=None, kwargs=None, **kw_coords):
+    def update_contour(self, ax, c, variable=None, coords=None, kwargs=None, **kw_coords):
         """Update a contour plot.
 
-        If a parameter is None, the value used for setting
-        the plot is used.
+        Parameters
+        ----------
+        ax: Matplotlib axis
+        c: Contour
+        variable: str, optional
+            If None, previously plotted is used.
+        coords: List[str], optional
+            Coordinates to use.
+            If None, selected coordinates with size higher
+            than 1 will be used.
+        kwargs: Dict[An]
+            Passed to contour.
+        kw_coords: Keys
+            Subset of data to plot.
+            If missing, last plotted is used.
         """
         if variable is None:
             variable = self.last_plot
         kw_coords = self.get_coords_full(**kw_coords)
-        kw_coords = self.sort_by_coords(kw_coords)
+        kw_coords = self._get_coords_none_previous(**kw_coords)
+        frame = self.select(variable, **kw_coords)
 
         self.set_plot_keys(variable, **kw_coords)
 
-        slice_lat = kw_coords['lat']
-        slice_lon = kw_coords['lon']
-        frame = self[variable][tuple(kw_coords.values())]
+        if coords is None:
+            coords = [name for name, key in kw_coords.key()
+                      if self.coords[name][key].size > 1]
+        values = [self.coords[name][kw_coords[name]]
+                  for name in coords]
 
         for coll in c.collections:
             ax.collections.remove(coll)
-
-        c = ax.contour(self.lon[slice_lon], self.lat[slice_lat],
-                       frame, **kwargs)
+        c = ax.contour(*values, frame, **kwargs)
 
         return c
 
-    def imshow_all(self, axes, variables=None, limits=None, kwargs=None, **kw_coords):
+    def imshow_all(self, axes, variables=None, coords=None, limits=None, kwargs=None, **kw_coords):
+        """Plot all variables.
+
+        Parameters
+        ----------
+        axes: Array of Matplotlib axis
+        variables: List[str]
+            List of variable to plot.
+            None elements will be skipped, and the
+            corresponding axe deleted.
+        coords: List[str], optional
+            Coordinate to plot along.
+            If None, selected coordinates with size higher
+            than 1 will be used.
+        limits: bool, optional
+            Set axis limits.
+        kwargs: Dict[Any]
+            Passed to imshow.
+        kw_coords: Keys
+            Subset of data to plot.
+
+        Returns
+        -------
+        Array of Matplotlib images.
+        """
         def plot(ax, dt, var, **kwargs):
             im_kw = {'vmin': dt.vi.get_attr_safe('vmin', var),
                      'vmax': dt.vi.get_attr_safe('vmax', var)}
@@ -187,7 +278,7 @@ class DataPlot(DataBase):
                 kwargs['kwargs'] = {}
             im_kw.update(kwargs.pop('kwargs'))
 
-            im = dt.imshow(ax, var, kwargs=im_kw, **kwargs)
+            im = dt.imshow(ax, var, coords=None, kwargs=im_kw, **kwargs)
             title = dt.vi.get_attr_safe('fullname', var, default=var)
             ax.set_title(title)
 
@@ -199,17 +290,35 @@ class DataPlot(DataBase):
 
             return im
 
-        images = self.iter_axes(axes, plot, variables,
+        images = self.iter_axes(axes, plot, variables, coords,
                                 limits=limits, kwargs=kwargs, **kw_coords)
         return images
 
     def update_imshow_all(self, axes, images, variables=None, **kw_coords):
+        """Update plots of multiple variables.
+
+        Parameters
+        ----------
+        axes: Array of Matplotlib axes
+        images: Array of Matplotlib images
+        variables: List[str]
+        kw_coords: Keys
+        """
         def update(ax, dt, var, im, **kw_coords):
             dt.update_imshow(im, var, **kw_coords)
         self.iter_axes(axes, update, variables, iterables=[images], **kw_coords)
 
     def del_axes_none(self, fig, axes, variables=None):
-        """Delete axes for which variables is None."""
+        """Delete axes for which variables is None.
+
+        Parameters
+        ----------
+        fig: Matplotlib figure
+        axes: Array of Matplotlib axes
+        variables: List[str]
+            List of variables. If element is None,
+            axis will be removed from figure.
+        """
         if variables is None:
             variables = self.vi.var
         variables = list(variables)
@@ -221,7 +330,23 @@ class DataPlot(DataBase):
                 fig.delaxes(ax)
 
     def iter_axes(self, axes, func, variables=None, iterables=None, **kwargs):
-        """Apply function over multiple axes."""
+        """Apply function over multiple axes.
+
+        Parameters
+        ----------
+        axes: Array of Matplotlib axis
+            Axes to iterate on.
+        func: Callable
+            Function to call for every axe.
+            func(ax, DataPlot, variable, *iterable, **kwargs)
+        variables: List[str]
+            None elements will be skipped.
+        iterables: List[List[Any]]
+            Argument passed to `func`, changing
+            for every axis.
+        kwargs: Any
+            Passed to func.
+        """
         if variables is None:
             variables = self.vi.var
         if iterables is None:
