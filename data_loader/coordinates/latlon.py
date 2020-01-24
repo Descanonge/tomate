fc371e00d6949317e7c43e15e960d443716b9e21
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2020-01-24 17:36:46 +0100

    Add specific init to latlon

--- data_loader/coordinates/latlon.py
+++ data_loader/coordinates/latlon.py
@@ -2,7 +2,13 @@
 
 from data_loader.coordinates.coord import Coord
 
+
 class Lat(Coord):
+    """Latitude coordinate."""
+
+    def __init__(self, name='lat', array=None,
+                 units='deg', fullname='Latitude', alt_name='latitude'):
+        super().__init__(name, array, units, alt_name, fullname)
 
     @staticmethod
     def format(value, fmt='.2f'):
@@ -17,7 +23,15 @@ class Lat(Coord):
         fmt = '{:%s}%s' % (fmt, end)
         return fmt.format(abs(value))
 
+
 class Lon(Coord):
+    """Longitude coordinate."""
+    # TODO: convert to km
+
+    def __init__(self, name='lon', array=None,
+                 units='deg', fullname='Longitude', alt_name='longitude'):
+        super().__init__(name, array, units, alt_name, fullname)
+
 
     @staticmethod
     def format(value, fmt='.2f'):
