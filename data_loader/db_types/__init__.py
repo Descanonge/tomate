ae757671842c248f6b0beb33b4ba89901a70014f
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2020-05-10 14:46:09 +0200

    Remove data_masked import

--- data_loader/db_types/__init__.py
+++ data_loader/db_types/__init__.py
@@ -2,14 +2,11 @@
 
 
 from .data_compute import DataCompute
-from .masked import data_masked
 from .masked.data_masked import DataMasked
 from .data_plot import DataPlot
 from .data_disk import DataDisk
 
 __all__ = [
-    'data_masked',
-
     'DataCompute',
     'DataMasked',
     'DataPlot',
