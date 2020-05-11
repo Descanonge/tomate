14711102757db0987f6ffbef1161ad089d277700
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2020-05-11 18:14:29 +0200

    Only import DataPlot if matplotlib present

--- data_loader/db_types/__init__.py
+++ data_loader/db_types/__init__.py
@@ -1,14 +1,18 @@
 """Collection of DataBase subclasses."""
 
 
+import importlib
+
 from .data_compute import DataCompute
 from .masked.data_masked import DataMasked
-from .data_plot import DataPlot
 from .data_disk import DataDisk
 
 __all__ = [
     'DataCompute',
     'DataMasked',
-    'DataPlot',
     'DataDisk'
 ]
+
+if importlib.util.find_spec('matplotlib') is not None:
+    from .plotting.data_plot import DataPlot
+    __all__.append('DataPlot')
