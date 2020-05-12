b3f4c5712cb20c0fdf337ef2faa88be5ec1e30f9
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2020-05-12 16:49:30 +0200

    Remove unused and add line plot

--- data_loader/db_types/plotting/data_plot.py
+++ data_loader/db_types/plotting/data_plot.py
@@ -17,6 +17,8 @@ from data_loader.keys.keyring import Keyring
 from data_loader.scope import Scope
 
 from data_loader.db_types.plotting.image import PlotObjectImage
+from data_loader.db_types.plotting.contour import PlotObjectContour
+from data_loader.db_types.plotting.line import PlotObjectLine
 
 
 log = logging.getLogger(__name__)
@@ -25,14 +27,35 @@ log = logging.getLogger(__name__)
 class DataPlot(DataBase):
     """Added functionalities for plotting data."""
 
+    def plot(self, ax, variable, data=None, axes=None, plot=True,
+             limits=True, kwargs=None, **keys):
+        self.check_loaded()
+        po = PlotObjectLine.create(self, ax, data=data, axes=axes, kwargs=kwargs,
+                                   var=variable, **keys)
+        if plot:
+            po.create_plot()
+        if limits:
+            po.set_limits()
 
         return po
 
-    def imshow(self, ax, variable, data=None, coords=None, plot=True,
+    def imshow(self, ax, variable, data=None, axes=None, plot=True,
                limits=True, kwargs=None, **keys):
         self.check_loaded()
-        po = PlotObjectImage.create(self, ax, data=data, coords=coords, kwargs=kwargs,
+        po = PlotObjectImage.create(self, ax, data=data, axes=axes, kwargs=kwargs,
                                     var=variable, **keys)
+        if plot:
+            po.create_plot()
+        if limits:
+            po.set_limits()
+
+        return po
+
+    def contour(self, ax, variable, data=None, axes=None, plot=True,
+                limits=True, kwargs=None, **keys):
+        self.check_loaded()
+        po = PlotObjectContour.create(self, ax, data=data, axes=axes, kwargs=kwargs,
+                                      var=variable, **keys)
         if plot:
             po.create_plot()
         if limits:
@@ -42,109 +65,6 @@ class DataPlot(DataBase):
 
 class DataPlot_(DataBase):
     """Added functionalities for plotting data."""
-
-    def imshow(self, ax, variable, data=None, coords=None, limits=True, kwargs=None, **kw_keys):
-        """Plot an image on a heatmap.
-
-        Parameters
-        ----------
-        ax: Matplotlib axis
-        variable: str
-        data: Array, optional
-            Data to use.
-        coords: List[str], optional
-            Coordinate to plot along.
-            If None, selected coordinates with size higher
-            than 1 will be used.
-        limits: bool, optional
-            Set axis limits.
-        kwargs: Dict[Any]
-            Passed to imshow.
-        kw_keys: key-like
-            Subset of data to plot.
-            Act on available.
-
-        Returns
-        -------
-        Matplotlib image.
-
-        Raises
-        ------
-        IndexError:
-            If image has wrong shape.
-        """
-        self.check_loaded()
-
-        keyring = Keyring(var=variable, **kw_keys)
-        keyring.make_full(self.dims)
-        keyring.make_total()
-        keyring.make_var_idx(self.loaded.var)
-
-        if coords is None:
-            coords = keyring.get_high_dim()[::-1]
-        self.plot_coords = coords
-
-        if data is not None:
-            image = data
-        else:
-            image = self.view_ordered(coords[::-1], keyring)
-        if image.ndim != 2:
-            raise IndexError("Selected data does not have the dimension"
-                             " of an image %s" % list(image.shape))
-
-        self.plotted = self.get_subscope('loaded', keyring)
-
-        kwargs_def = {'origin': 'lower'}
-        if kwargs is not None:
-            kwargs_def.update(kwargs)
-        kwargs = kwargs_def
-
-        extent = self.plotted.get_extent(*coords)
-        im = ax.imshow(image, extent=extent, **kwargs)
-
-        if limits:
-            self.set_limits(ax, *coords, scope=self.plotted)
-
-        return im
-
-    def update_imshow(self, im, variable=None, data=None, **keys):
-        """Update a heatmap plot.
-
-        If a parameter is None, the value used for setting
-        the plot is used.
-
-        Parameters
-        ----------
-        im: Matplotlib image
-        variable: str
-            If None, last plotted is used.
-        kw_keys: Keys
-            Subset of data to plot.
-            Act on available.
-            If missing, last plotted is used.
-
-        Raises
-        ------
-        IndexError:
-            If image has wrong shape.
-        """
-        if variable is None:
-            variable = self.plotted.var[0]
-        self.plotted = self.get_subscope('loaded', var=variable, **keys)
-
-        coords = self.plot_coords
-
-        if data is not None:
-            image = data
-        else:
-            image = self.view_ordered(coords[::-1], var=variable, **keys)
-        if image.ndim != 2:
-            raise IndexError("Selected data does not have the dimension"
-                             " of an image %s" % str(image.shape))
-        im.set_data(image)
-
-        # TODO: colorbar
-
     def contour(self, ax, variable, coords=None, limits=True, kwargs=None, **keys):
         """Plot contour of a variable.
 
@@ -273,11 +193,11 @@ class DataPlot_(DataBase):
             title = dt.vi.get_attr_safe('fullname', var, default=var)
             ax.set_title(title)
 
-            if _has_matplotlib:
-                divider = make_axes_locatable(ax)
-                cax = divider.append_axes("right", "2%", 0)
-                label = dt.vi.get_attr_safe('units', var, default='')
-                ax.get_figure().colorbar(im, cax=cax, label=label)
+            # if _has_matplotlib:
+            #     divider = make_axes_locatable(ax)
+            #     cax = divider.append_axes("right", "2%", 0)
+            #     label = dt.vi.get_attr_safe('units', var, default='')
+            #     ax.get_figure().colorbar(im, cax=cax, label=label)
 
             return im
 
@@ -362,11 +282,6 @@ class DataPlot_(DataBase):
         output = np.array(output)
         output = np.reshape(output, axes.shape)
         return output
-
-    def hoevmuller(self):
-        pass
-
-    # TODO: hoevmuller plot
 
     def plot_histogram(self, ax, variable, kwargs=None, **keys):
         if kwargs is None:
