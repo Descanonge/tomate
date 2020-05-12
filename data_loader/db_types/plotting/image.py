12de41e16323259a8a464ad8b5f41a4a6e039bc5
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2020-05-12 16:46:57 +0200

    Up plot object
    Better axes management
    
    Setting limits left to subclasses

--- data_loader/db_types/plotting/image.py
+++ data_loader/db_types/plotting/image.py
@@ -7,23 +7,46 @@ from data_loader.keys.keyring import Keyring
 
 class PlotObjectImage(PlotObjectABC):
 
+    DIM = 2
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.kwargs = {'origin': 'lower'}
+        self.set_kwargs(origin='lower')
 
     @property
     def image(self):
         return self.object
 
     def _get_data(self):
-        image = self.db.view_ordered(self.coords, keyring=self.scope.parent_keyring)
-        if image.ndim != 2:
-            raise IndexError("Selected data does not have the dimension"
-                             " of an image %s" % list(image.shape))
+        image = self.db.view_ordered(self.axes[1::-1],
+                                     keyring=self.keyring)
         return image
 
+    def find_axes(self, axes=None):
+        if axes is not None:
+            axes_ = axes
+            if len(axes_) not in [2, 3]:
+                raise ValueError("Number of axes supplied not 2 or 3"
+                                 " (supplied %s)" % axes_)
+        else:
+            axes_ = self.keyring.get_high_dim()[::-1]
+
+        if len(axes_) == 2:
+            axes_.append(self.scope.var[0])
+
+        return axes_
+
     def create_plot(self):
-        self.db.check_loaded()
         image = self.get_data()
-        extent = self.scope.get_extent(*self.coords)
+        extent = self.scope.get_extent(*self.axes[:2])
         self.object = self.ax.imshow(image, extent=extent, **self.kwargs)
+
+    def update_plot(self, **keys):
+        self.up_scope(**keys)
+        image = self.get_data()
+        self.image.set_data(image)
+        self.image.set_extent(self.scope.get_extent(*self.axes[:2]))
+
+    def set_limits(self):
+        for x, dim in zip([True, False], self.axes[:2]):
+            self._set_limits_dim(dim, x)
--- data_loader/db_types/plotting/plot_object.py
+++ data_loader/db_types/plotting/plot_object.py
@@ -4,7 +4,8 @@ from typing import Any, Dict, List, Union, TYPE_CHECKING
 
 from matplotlib.axes import Axes
 
-from data_loader.custom_types import KeyLikeInt
+from data_loader.custom_types import KeyLikeInt, KeyLikeValue
+from data_loader.keys.key import KeyValue
 from data_loader.keys.keyring import Keyring
 from data_loader.scope import Scope
 
@@ -15,52 +16,78 @@ if TYPE_CHECKING:
 class PlotObjectABC():
     """Object containing information about plots."""
 
+    DIM = 0
+
     def __init__(self, db: 'DataPlot', ax: Axes,
-                 scope: Scope, coords: List[str],
+                 scope: Scope, axes: List[str],
                  data, **kwargs):
         self.db = db
         self.ax = ax
         self.scope = scope
         self.object = None
         self.data = data
-        self.coords = coords
+        self.axes = axes
         self.kwargs = kwargs
 
-    def _up_scope(self, keyring=None, **keys):
-        scope = self.db.get_subscope(self.scope, keyring=keyring,
+    @property
+    def keyring(self):
+        return self.scope.parent_keyring
+
+    def up_scope(self, **keys):
+        keyring = self.keyring
+        for dim, key in keys.items():
+            keyring[dim] = key
+        self.reset_scope(keyring)
+
+    def up_scope_by_value(self, **keys):
+        keys_ = {}
+        for dim, key in keys.items():
+            keys_[dim] = KeyValue(key).apply(self.scope.dims[dim])
+        self.up_scope(**keys_)
+
+    def reset_scope(self, keyring=None, **keys):
+        scope = self.db.get_subscope(self.scope.parent_scope,
+                                     keyring=keyring,
                                      int2list=False, **keys)
         self.scope = scope
 
-    def _up_scope_by_value(self, keyring=None, **keys):
-        scope = self.db.get_subscope_by_value(self.scope,
-                                              keyring=keyring,
+    def reset_scope_by_value(self, **keys):
+        scope = self.db.get_subscope_by_value(self.scope.parent_scope,
                                               int2list=False, **keys)
         self.scope = scope
 
     def get_data(self):
         if self.data is not None:
             return self.data
+        self.check_keyring()
         return self._get_data()
 
     def _get_data(self):
         raise NotImplementedError()
 
+    def check_keyring(self):
+        dim = len(self.keyring.get_high_dim())
+        if dim != self.DIM:
+            raise IndexError("Data to plot does not have right dimension"
+                             " (is %d, expected %d)" % (dim, self.DIM))
+
+    def find_axes(self, axes=None):
+        raise NotImplementedError()
+
     @classmethod
     def create(cls, db: 'DataPlot', ax: Axes,
                scope: Union[str, Scope] = 'loaded',
-               coords: List[str] = None,
+               axes: List[str] = None,
                data=None,
                kwargs: Dict[str, Any] = None,
                **keys: KeyLikeInt):
-        scope = db.get_scope(scope).copy()
+        scope = db.get_subscope(scope, name='plotted').copy()
         scope.slice(**keys, int2list=False)
 
-        if coords is None:
-            coords = scope.parent_keyring.get_high_dim()
         if kwargs is None:
             kwargs = {}
-
-        po = cls(db, ax, scope, coords, data, **kwargs)
+        po = cls(db, ax, scope, axes, data, **kwargs)
+        po.axes = po.find_axes(axes)
         return po
 
     def set_kwargs(self, replace=True, **kwargs):
@@ -70,30 +97,55 @@ class PlotObjectABC():
             kwargs.update(self.kwargs)
             self.kwargs = kwargs
 
-    def set_plot(self, *args, **keys):
+    def set_plot(self):
+        if self.object is None:
+            self.create_plot()
+        else:
+            self.update_plot()
+
+    def create_plot(self):
         raise NotImplementedError()
 
     def remove(self):
-        raise NotImplementedError()
+        self.object.remove()
+        self.object = None
 
-    def update_plot(self, *args, **keys):
-        raise NotImplementedError()
+    def update_plot(self, **keys):
+        self.up_scope(**keys)
+        self.remove()
+        self.create_plot()
 
     def set_limits(self):
-        set_limits(self.ax, self.scope, self.coords)
+        raise NotImplementedError()
 
+    def _set_limits_dim(self, dim, x=True):
+        limits = self.scope[dim].get_limits()
+        if x:
+            self.ax.set_xlim(*limits)
+        else:
+            self.ax.set_ylim(*limits)
+        print('dim ', x, *limits)
 
-def set_limits(ax, scope, coords=None, keyring=None, **keys):
+    def _set_limits_var(self, var, x=True):
+        vmin = self.db.vi.get_attr_safe(var, 'vmin')
+        vmax = self.db.vi.get_attr_safe(var, 'vmax')
+        print('var ', x, vmin, vmax)
+        if x:
+            self.ax.set_xlim(vmin, vmax)
+        else:
+            self.ax.set_ylim(vmin, vmax)
+
+def set_limits_dim(ax, scope, axes=None, keyring=None, **keys):
     keyring = Keyring.get_default(keyring, **keys)
-    if coords:
-        names = coords
+    if axes:
+        names = axes
     elif keyring:
         names = keyring.dims[:2]
     else:
         names = [c.name for c in scope.coords if c.size > 1]
     keyring.make_full(names)
     keyring.make_total()
-    axes = [ax.xaxis, ax.yaxis]
-    for axis, name in zip(axes[:len(names)], names):
+    funcs = [ax.set_xlim, ax.set_ylim]
+    for f, name in zip(funcs[:len(names)], names):
         limits = scope[name].get_limits(keyring[name].value)
-        axis.set_view_interval(*limits)
+        f(*limits)
