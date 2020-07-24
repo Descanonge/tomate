2027f4f877ca21dfc1125076ec1775f8e8de75ff
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2020-07-24 23:42:36 +0200

    Add Key test template

---
+++ tests/unit/keys/test_key.py
@@ -0,0 +1,26 @@
+
+from tomate.keys.key import Key, reverse_slice_order
+
+
+def test_reverse_slice():
+    pass
+
+
+def test_list2slice():
+    pass
+
+
+def test_guess_size():
+    pass
+
+
+def test_guess_tolist():
+    pass
+
+
+def test_multiplication():
+    pass
+
+
+def test_addition():
+    pass
